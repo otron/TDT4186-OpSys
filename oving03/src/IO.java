b3b6ed615286bda1984e271a845f5d4a5903eee6
otron <oddweb42+gith@gmail.com>
2012-10-17 17:41:44 +0200

    constructed constructor for IO. commenting like a mad cunt.

--- oving03/src/IO.java
+++ oving03/src/IO.java
@@ -1,5 +1,29 @@
 
 public class IO {
+	/** yo yo wat up some stats yo */
+	private Statistics stats;
+	/** get some gu-gu-G.U.I. */
+	private Gui gui;
 	
+	/** This is the queue containing processes wanting to get their filthy hands on some I/O */
+	private Queue ioQueue;
+	/** The process which currently has right of I/O */
+	private Process activeProc = null;
+	/** the time to wait (in ms) until the I/O becomes available */
+	private long ioWait;
+	
+	/**
+	 * This is the constructooorrrr. It ain't nuthin' fancy.
+	 * @param statistics
+	 * @param gui
+	 * @param IOQueue
+	 * @param IOWait
+	 */
+	public IO(Statistics statistics, Gui gui, Queue IOQueue, long IOWait) {
+		this.stats = statistics;
+		this.gui = gui;
+		this.ioQueue = IOQueue;
+		this.ioWait = IOWait;
+	}
 
 }
